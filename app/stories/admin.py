bc327ad88abf20e793f966a48a3d1308808984b3
Serbic Marko <marko.serbic@gmail.com>
2020-07-12 15:08:57 +0200

    add story-node with connection logic

--- app/stories/admin.py
+++ app/stories/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Node
 
 admin.site.register(Post)
+admin.site.register(Node)
--- app/stories/models.py
+++ app/stories/models.py
@@ -4,15 +4,28 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+
+class Node(models.Model):
+    content = models.TextField()
+    action = models.CharField(max_length=100, null=True)
+    # rating = models.FloatField() # TODO: add this to another table
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    child_nodes = models.ManyToManyField("self", symmetrical=False)
+
+    def __str__(self):
+        return f"Story Node after action: '{self.action}', by author: {self.author}"
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    description = models.TextField(default="")
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # rating = models.FloatField() # TODO: add this to another table
+    first_node = models.ForeignKey(Node, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("post-detail", kwargs={"pk": self.pk})
-    
